6209f0588e3ee7f3039a8308b781abfb8123b112
Seif Tariq <SeifTariq@gmail.com>
2024-10-06 17:51:10 +0300

    finish splash view

---
+++ lib/Core/Utils/assets.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBackground
+  /// assets/images/background.svg
+  static const String imagesBackground = "assets/images/background.svg";
+
+  /// Assets for imagesSplash
+  /// assets/images/splash.png
+  static const String imagesSplash = "assets/images/splash.png";
+}
+
--- lib/Features/Splash Screen/splash_view.dart
+++ lib/Features/Splash Screen/splash_view.dart
@@ -1,10 +1,27 @@
+import 'package:coffee_oasis/Core/Utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      
+      body: Container(
+        decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 58, 38, 28),
+          image: DecorationImage(
+            image: AssetImage(Assets.imagesSplash),
+            
+          )
+        ),
+      ),
+    );
   }
 }
