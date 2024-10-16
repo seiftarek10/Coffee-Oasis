f3ff5da116cdab6afbcc1dd289cd1d07e56a88c6
Seif Tariq <SeifTariq@gmail.com>
2024-10-16 19:06:37 +0300

    apply flavors

--- lib/Core/Routes/app_router.dart
+++ lib/Core/Routes/app_router.dart
@@ -5,67 +5,83 @@ import 'package:coffee_oasis/Features/Auth/Presentation/Views/Screens/sign_up_vi
 import 'package:coffee_oasis/Features/Delivery/Presentation/Views/delivery.dart';
 import 'package:coffee_oasis/Features/Owner/Presentation/Views/Screens/category_view.dart';
 import 'package:coffee_oasis/Features/Owner/Presentation/Views/owner.dart';
+import 'package:coffee_oasis/Features/Splash%20Screen/splash_view.dart';
 import 'package:coffee_oasis/Features/Staff/Presentation/Views/Screens/staff_home_view.dart';
 import 'package:coffee_oasis/Features/Staff/Presentation/Views/Screens/staff_order_details.dart';
 import 'package:coffee_oasis/Features/User/Presentation/Views/Screens/coffee_details_view.dart';
 import 'package:coffee_oasis/Features/User/Presentation/Views/Screens/favorite_view.dart';
 import 'package:coffee_oasis/Features/User/Presentation/Views/Screens/order_details.dart';
 import 'package:coffee_oasis/Features/User/Presentation/Views/user.dart';
+import 'package:coffee_oasis/coffee_oasis.dart';
 import 'package:go_router/go_router.dart';
 
-final GoRouter router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
+class AppRouter {
+  final Flavor flavor;
+
+  AppRouter({required this.flavor});
+
+
+   GoRouter get router{
+    return GoRouter(
+      initialLocation: '/',
+    routes: <RouteBase>[
+      GoRoute(
+        
         path: '/',
-        pageBuilder: (context, state) => 
-            pageTransition(context, state, const DeliveryView())),
-    // GoRoute(
-    //     path: '/',
-    //     pageBuilder: (context, state) =>
-    //         pageTransition(context, state, const SplashView())),
-    GoRoute(
-        path: Routes.owner,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const OwnerView())),
-    GoRoute(
-        path: Routes.category,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const CategoryView())),
-    GoRoute(
-        path: Routes.user,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const UserView())),
-    GoRoute(
-        path: Routes.coffeeDetails,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const CoffeeDetailsView())),
-    GoRoute(
-        path: Routes.orderDetails,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const OrderDetailsView())),
-    GoRoute(
-        path: Routes.favorite,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const FavoriteView())),
-    GoRoute(
-        path: Routes.signup,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const SignUpView())),
-    GoRoute(
-        path: Routes.signin,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const SignInView())),
-    GoRoute(
-        path: Routes.staffOrderDetails,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const StaffOrderDetailsView())),
-       GoRoute(
-        path: Routes.staff,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const StaffHomeView())),
-       GoRoute(
-        path: Routes.delivery,
-        pageBuilder: (context, state) =>
-            pageTransition(context, state, const DeliveryView()))
-  ],
-);
+        
+        pageBuilder: (context, state) {
+          return pageTransition(
+            context,
+            state,
+            SplashView(flavor: flavor),
+          );
+        },
+      ),
+      GoRoute(
+          path: Routes.owner,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const OwnerView())),
+      GoRoute(
+          path: Routes.category,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const CategoryView())),
+      GoRoute(
+          path: Routes.user,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const UserView())),
+      GoRoute(
+          path: Routes.coffeeDetails,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const CoffeeDetailsView())),
+      GoRoute(
+          path: Routes.orderDetails,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const OrderDetailsView())),
+      GoRoute(
+          path: Routes.favorite,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const FavoriteView())),
+      GoRoute(
+          path: Routes.signup,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const SignUpView())),
+      GoRoute(
+          path: Routes.signin,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const SignInView())),
+      GoRoute(
+          path: Routes.staffOrderDetails,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const StaffOrderDetailsView())),
+      GoRoute(
+          path: Routes.staff,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const StaffHomeView())),
+      GoRoute(
+          path: Routes.delivery,
+          pageBuilder: (context, state) =>
+              pageTransition(context, state, const DeliveryView()))
+    ],
+  );
+   }
+}
--- lib/Features/Splash Screen/splash_view.dart
+++ lib/Features/Splash Screen/splash_view.dart
@@ -1,11 +1,14 @@
 import 'package:coffee_oasis/Core/Routes/routes_keys.dart';
 import 'package:coffee_oasis/Core/Utils/assets.dart';
 import 'package:coffee_oasis/Core/Widgets/Animation/opacity.dart';
+import 'package:coffee_oasis/coffee_oasis.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class SplashView extends StatefulWidget {
-  const SplashView({super.key});
+  const SplashView({super.key, required this.flavor});
+
+  final Flavor flavor;
 
   @override
   State<SplashView> createState() => _SplashViewState();
@@ -14,10 +17,25 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   void _navigation() {
     Future.delayed(const Duration(milliseconds: 500), () {
-      if (mounted) {
-        GoRouter.of(context).pushReplacement(Routes.user);
-      }
+      nextPage(widget.flavor);
     });
+  }
+
+  void nextPage(Flavor flavor) {
+    switch (flavor) {
+      case Flavor.user:
+        GoRouter.of(context).pushReplacement(Routes.user);
+        break;
+      case Flavor.owner:
+        GoRouter.of(context).pushReplacement(Routes.owner);
+        break;
+      case Flavor.delivery:
+        GoRouter.of(context).pushReplacement(Routes.delivery);
+        break;
+      case Flavor.staff:
+        GoRouter.of(context).pushReplacement(Routes.staff);
+        break;
+    }
   }
 
   @override
