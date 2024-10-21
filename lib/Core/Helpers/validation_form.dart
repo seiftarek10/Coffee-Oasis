c86f4730135a56e720cf94b8a19f6a021c32148b
Seif Tariq <SeifTariq@gmail.com>
2024-10-21 15:18:37 +0300

    add validation form class

---
+++ lib/Core/Helpers/validation_form.dart
@@ -0,0 +1,23 @@
+abstract class ValidationForm {
+  static String? nullOrEpmty(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Required';
+    }
+    return null;
+  }
+
+  static String? validEmail(String? email) {
+    if (!RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+        .hasMatch(email ?? '')) {
+      return 'Unvalid Email';
+    }
+    return null;
+  }
+
+  static String? validPhoneNumber(String? phoneNumber) {
+    if (!RegExp(r'^(010|011|012|015)[0-9]{8}$').hasMatch(phoneNumber ?? '')) {
+      return 'Enter Valid Number';
+    }
+    return null;
+  }
+}
